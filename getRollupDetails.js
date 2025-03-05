9bce4499657deec0fc648d026d2dddb4b46ce207
0xrouss <pablovigara@gmail.com>
2025-03-05 19:28:31 +0100

    check last verification timestamp

--- getRollupDetails.js
+++ getRollupDetails.js
@@ -25,9 +25,7 @@ for (let i = 1; i <= rollupCount; i++) {
         functionName: "rollupIDToRollupData",
         args: [i],
     });
-    console.log();
 
-    console.log("Indexed ID: ", i);
     const networkName = await readContract(publicClient, {
         address: rollupData.rollupContract,
         abi: polygonZkEVMEtrogABI,
@@ -46,9 +44,35 @@ for (let i = 1; i <= rollupCount; i++) {
         functionName: "gasTokenNetwork",
     });
 
-    console.log("Rollup Contract: ", rollupData.rollupContract);
-    console.log("Chain ID: ", parseInt(rollupData.chainID));
-    console.log("Network Name: ", networkName);
-    console.log("Gas Token Address: ", gasTokenAddress);
-    console.log("Gas Token Network: ", gasTokenNetwork);
+    if (rollupData.lastVerifiedBatch != 0) {
+        const sequencedBatchData = await readContract(publicClient, {
+            address: "0x5132A183E9F3CB7C848b0AAC5Ae0c4f0491B7aB2",
+            abi: polygonRollupManagerABI,
+            functionName: "getRollupSequencedBatches",
+            args: [i, rollupData.lastVerifiedBatch],
+        });
+
+        const lastVerifiedTimestamp = parseInt(sequencedBatchData.sequencedTimestamp) * 1000;
+        const currentTime = Date.now();
+        const timeDiff = currentTime - lastVerifiedTimestamp;
+
+        // Convert time difference to days, hours, minutes
+        const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+
+        if (days <= 1) {
+            console.log("Indexed ID: ", i);
+            console.log("Rollup Contract: ", rollupData.rollupContract);
+            console.log("Chain ID: ", parseInt(rollupData.chainID));
+            console.log("Network Name: ", networkName);
+            console.log("Gas Token Address: ", gasTokenAddress);
+            console.log("Gas Token Network: ", gasTokenNetwork);
+            console.log("Last Batch Sequenced: ", parseInt(rollupData.lastBatchSequenced));
+            console.log("Last Verified Batch: ", parseInt(rollupData.lastVerifiedBatch));
+            console.log("Last Verified Batch Timestamp: ", new Date(lastVerifiedTimestamp).toISOString());
+            console.log("Time since last verification: ", `${days}d ${hours}h ${minutes}m`);
+            console.log();
+        }
+    }
 }
